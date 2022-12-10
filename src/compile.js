412b47a922b4ca5cf816cce81ac2d1f57840cfb0
SithLor <84148006+SithLor@users.noreply.github.com>
2022-12-10 10:35:04 +0000

    d

---
+++ src/compile.js
@@ -0,0 +1,5 @@
+const bytenode = require('bytenode')
+let compiledFilename = bytenode.compileFile({
+    filename: './target.js',
+    output: './target.jsc' // if omitted, it defaults to '/path/to/your/file.jsc'
+});
---
+++ src/target.js
@@ -0,0 +1,3 @@
+import{ node_imp } from "./node_imp.js";
+
+console.log(node_imp)
